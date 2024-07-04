f79a443f370b434b6a201200401027dfe77f7912
SandeepPuri4 <sndpp21@gmail.com>
2024-07-04 13:22:37 +0530

    jkjj

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -22,11 +22,11 @@ export default function MainSection() {
           <div className="md:w-[80rem] md:flex ">
       <div className="mt-0 h-[400px] md:h-[510px] flex flex-col md:flex md:flex-row md:mt-24 ">
           <div className="mt-5 flex flex-col pl-2 md:mt-14">
-            <h1 className="text-[40px] text-green-950 pl-5 font-semibold  md:text-[70px] md:w-[650px]">Get Skilled & Boost</h1>
-            <h1  className="text-[40px] pl-20 text-green-950 font-semibold  md:text-[70px] md:w-[650px]">Your Career</h1>
+            <h1 className="text-[30px] text-green-950 pl-10 font-semibold  md:text-[70px] md:w-[650px]">Get Skilled & Boost</h1>
+            <h1  className="text-[30px] pl-24 text-green-950 font-semibold  md:text-[70px] md:w-[650px]">Your Career</h1>
             <div className=" w-[100%] h-[220px] md:w-[700px]">
               
-            <p className="w-[98%] px-1 ml-1 mt-4 font-[100px] text-stone-700 text-[17px] ">Enroll in our programs built with top university and industry partners, with dedicated mentorship and expert career guidance.,We partner with companies and individuals to address their unique needs, providing training and coaching that helps working professionals achieve their career goals</p>
+            <p className="w-[98%] px-1 ml-1 mt-4 text-[13px] text-stone-700 md:text-[17px] ">Enroll in our programs built with top university and industry partners, with dedicated mentorship and expert career guidance.,We partner with companies and individuals to address their unique needs, providing training and coaching that helps working professionals achieve their career goals</p>
             </div>
             <div className=" w-full mx-auto px-auto">
               <button onClick={enquiryHandler} className="bg-green-600 mx-auto ml-28 mt-0 text-white px-7 py-2 rounded-3xl hover:bg-green-500] hover:bg-green-500 ">ENQUIRY NOW</button>
