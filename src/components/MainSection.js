f2504825bc4c3f47997098ad06fd58d29f3f4d2c
SandeepPuri4 <sndpp21@gmail.com>
2024-07-06 11:08:38 +0530

    kk

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -21,15 +21,15 @@ export default function MainSection() {
          </div> */}
          <div className="h-[435px] w-[415px]   bg-blue-50 md:bg-white md:h-[700px] flex mx-auto md:w-[75rem] md:mx-auto">
           <div className="  w-[415px]  md:w-[80rem] md:flex ">
-      <div className="mt-0 h-[420px] md:h-[510px] flex flex-col md:flex md:flex-row md:mt-24 ">
-          <div className="mt-5 h-[400px] flex flex-col md:mt-14">
+      <div className="mt-0 h-[400px] md:h-[510px] flex flex-col md:flex md:flex-row md:mt-24 ">
+          <div className="mt-5 flex flex-col md:mt-14">
             <h1 className="text-[30px] text-green-950 mx-auto  font-semibold  md:text-[70px] md:w-[650px]">Get Skilled & Boost</h1>
             <h1  className="text-[30px] mx-auto text-green-950 font-semibold  md:text-[70px] md:w-[650px] md:mx-auto md:px-24">Your Career</h1>
             <div className="block md:hidden">
-            <div className=" w-[410px] mt-6 h-[180px] md:hidden">
+            <div className=" w-[415px] mt-6 h-[180px] md:hidden">
               <p className=" text-[16px] px-10">Enroll in our programs built with top university</p>
-              <p className="text-[16px] px-7">and industry partners, with dedicated mentorship </p>
-              <p className="text-[16px] px-11">and expert career guidance.,We partner with</p>
+              <p className="text-[16px] px-8">and industry partners, with dedicated mentorship </p>
+              <p className="text-[16px] px-12">and expert career guidance.,We partner with</p>
               <p className="text-[16px] px-12">companies and individuals to address their</p>
               <p className="text-[16px] px-10">unique needs, providing training and coaching</p>
               <p className="text-[16px] px-11">that helps working professionals achieve their</p>
@@ -42,7 +42,7 @@ export default function MainSection() {
             <p className="text-[16px]">Enroll in our programs built with top university and industry partners, with dedicated mentorship and expert career guidance.,We partner with companies and individuals to address their unique needs, providing training and coaching that helps working professionals achieve their career goals.</p>
             </div>
             </div>
-            <div className=" mt-12 mx-auto md:mx-16">
+            <div className=" mt-8 mx-auto md:mx-16">
               <button onClick={enquiryHandler} className="bg-green-600  mx-auto mt-0 text-white px-7 py-2 rounded-3xl hover:bg-green-500] hover:bg-green-500 ">ENQUIRY NOW</button>
             </div>
             <Form />
